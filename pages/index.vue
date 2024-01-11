e62563b09fe48cc1ff593fdc09e80b39e3ff2e95
Lukas Grimm <lukaskingsmail@gmail.com>
2024-01-11 17:12:51 +0100

    Fix dark mode

--- pages/index.vue
+++ pages/index.vue
@@ -5,17 +5,17 @@
                 {{ content!.name }}
             </h1>
         </div>
-        <h2 class="border border-rose-200 p-4 rounded-xl text-2xl md:text-4xl font-medium md:col-span-3">
+        <h2 class="bento-card rounded-xl text-2xl md:text-4xl font-medium md:col-span-3">
             {{ content!.role }}
         </h2>
-        <div class="border border-rose-200 p-4 rounded-xl">
-            <div class="text-gray-700">Experience</div>
+        <div class="bento-card rounded-xl">
+            <div class="text-zinc-700 dark:text-zinc-400">Experience</div>
             <div class="text-xl">
                 {{ content!.experience }}
             </div>
         </div>
-        <div class="border border-rose-200 p-4 rounded-xl md:col-span-2">
-            <div class="text-gray-700">Education</div>
+        <div class="bento-card rounded-xl md:col-span-2">
+            <div class="text-zinc-700 dark:text-zinc-400">Education</div>
             <div class="text-xl">
                 {{ content!.education }}
             </div>
@@ -25,19 +25,19 @@
                 {{ content!.location }}
             </div>
         </div>
-        <div class="border border-rose-200 p-4 rounded-xl md:col-span-3 md:row-span-2">
-            <div class="text-gray-700 mb-2">About me</div>
+        <div class="bento-card rounded-xl md:col-span-3 md:row-span-2">
+            <div class="text-gray-700 dark:text-zinc-400 mb-2">About me</div>
             <div>
                 {{ content!.about }}
             </div>
         </div>
-        <a :href="content!.github" class="border border-rose-200 px-4 py-2 rounded-xl col-span-1 row-span-1">
+        <a :href="content!.github" class="bento-card !px-4 !py-2 rounded-xl col-span-1 row-span-1">
             GitHub
         </a>
-        <a :href="content!.linkedIn" class="border border-rose-200 px-4 py-2 rounded-xl col-span-1 row-span-1">
+        <a :href="content!.linkedIn" class="bento-card !px-4 !py-2 rounded-xl col-span-1 row-span-1">
             LinkedIn
         </a>
-        <NuxtLink to="/resume" class="border border-rose-200 px-4 py-2 rounded-xl col-span-1 row-span-1">
+        <NuxtLink to="/resume" class="bento-card !px-4 !py-2 rounded-xl col-span-1 row-span-1">
             Resumé
         </NuxtLink>
     </div>
@@ -56,4 +56,17 @@ type BiograhphyContent = {
 };
 
 const { data: content } = await useAsyncData('biography', () => queryContent<BiograhphyContent>('biography').findOne())
+
+useHead({
+    title: `${content.value?.name} - ${content.value?.role}`,
+    meta: [
+        { name: 'description', content: content.value?.about }
+    ],
+})
 </script>
+
+<style lang="postcss" scoped>
+.bento-card {
+    @apply dark:bg-zinc-950 border dark:border-rose-300 border-rose-200 px-4 py-6;
+}
+</style>
