0c76cf4a6d50f69963c8375198679d63e0ba8581
Lukas Grimm <lukaskingsmail@gmail.com>
2024-01-11 20:26:59 +0100

    Add icon to links

--- pages/index.vue
+++ pages/index.vue
@@ -12,12 +12,22 @@
         </LandingCard>
         <LandingCard class="!p-0 md:col-span-3">
             <NuxtLink to="/resume" class="block !px-4 !py-2">
-                See Resumé
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 inline-block">
+                    <path fill-rule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                </svg>
+                <span class="align-middle">
+                    See Resumé
+                </span>
             </NuxtLink>
         </LandingCard>
         <LandingCard class="!p-0 md:col-span-3">
-            <NuxtLink to="/projects" class="block !px-4 !py-2 ">
-                Explore Projects
+            <NuxtLink to="/projects" class="block !px-4 !py-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 inline-block">
+                    <path fill-rule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                </svg>
+                <span class="align-middle">
+                    Explore Projects
+                </span>
             </NuxtLink>
         </LandingCard>
         <LandingCard class="md:col-span-6">
