ab5a17d26ed48aaba9641be73cb5d44522864d0a
Lukas Grimm <lukaskingsmail@gmail.com>
2024-01-11 16:45:41 +0100

    Add social buttons to landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,59 @@
 <template>
-    <div class="max-w-[820px] mx-4 md:mx-auto grid grid-cols-6 gap-4">
-        <div class="relative col-span-3 rounded-xl row-span-3">
-            <img
-                class="rounded-xl"
-                src="/profile.jpg"
-                alt="Profile Picture"
-            >
+    <div class="max-w-[820px] mx-4 md:mx-auto grid grid-rows-[auto_auto_1fr_auto_auto] grid-cols-6 gap-4">
+        <div class="min-h-[400px] relative col-span-3 rounded-xl row-span-3 bg-[url('/profile.jpg')] bg-cover">
             <h1 class="absolute bottom-0 left-0 right-0 text-center p-3 rounded-b-xl backdrop-blur-lg text-white text-6xl font-bold">
-                {{ content.name }}
+                {{ content!.name }}
             </h1>
         </div>
         <h2 class="border border-rose-200 p-4 rounded-xl text-4xl font-medium col-span-3">
-            {{ content.role }}
+            {{ content!.role }}
         </h2>
         <div class="border border-rose-200 p-4 rounded-xl">
             <div class="text-gray-700">Experience</div>
             <div class="text-xl">
-                {{ content.experience }}
+                {{ content!.experience }}
             </div>
         </div>
         <div class="border border-rose-200 p-4 rounded-xl col-span-2">
             <div class="text-gray-700">Education</div>
             <div class="text-xl">
-                {{ content.education }}
+                {{ content!.education }}
             </div>
         </div>
-        <div class="relative col-span-3 rounded-xl row-span-2">
-            <img
-                class="rounded-xl"
-                src="/map.png"
-                alt="Profile Picture"
-            >
+        <div class="relative col-span-3 rounded-xl row-span-2 bg-[url('/map.png')] bg-cover">
             <div class="absolute bottom-0 left-0 right-0 text-center p-3 rounded-b-xl backdrop-blur-lg text-white text-xl">
-                {{ content.location }}
+                {{ content!.location }}
             </div>
         </div>
-        <div class="border border-rose-200 p-4 rounded-xl col-span-3">
+        <div class="border border-rose-200 p-4 rounded-xl col-span-3 row-span-2">
             <div class="text-gray-700 mb-2">About me</div>
             <div>
-                {{ content.about }}
+                {{ content!.about }}
             </div>
         </div>
+        <a :href="content!.github" class="border border-rose-200 px-4 py-2 rounded-xl col-span-1 row-span-1">
+            GitHub
+        </a>
+        <a :href="content!.linkedIn" class="border border-rose-200 px-4 py-2 rounded-xl col-span-1 row-span-1">
+            LinkedIn
+        </a>
+        <NuxtLink to="/resume" class="border border-rose-200 px-4 py-2 rounded-xl col-span-1 row-span-1">
+            Resumé
+        </NuxtLink>
     </div>
 </template>
 
 <script setup lang="ts">
-const content = await queryContent('/biography').findOne()
-console.log(content);
+type BiograhphyContent = {
+    name: string;
+    role: string;
+    experience: string;
+    education: string;
+    location: string;
+    about: string;
+    github: string;
+    linkedIn: string
+};
+
+const { data: content } = await useAsyncData('biography', () => queryContent<BiograhphyContent>('biography').findOne())
 </script>
