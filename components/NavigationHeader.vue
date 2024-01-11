5c065150c1355d69f80fb0fe19ec4b78b00336bb
Lukas Grimm <lukaskingsmail@gmail.com>
2024-01-11 15:12:19 +0100

    Extract navigation to component

---
+++ components/NavigationHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav class="bg-gradient-to-r from-rose-200 via-amber-200 to-green-200 md:max-w-[820px] px-1.5 py-1.5 rounded-xl my-3 mx-4 md:mx-auto">
+        <ul class="flex justify-start">
+            <li v-for="link in links" class="px-3 py-1.5 hover:backdrop-saturate-200 hover:backdrop-brightness-90 rounded-lg">
+                <NuxtLink :to="link.to">
+                    {{ link.name }}
+                </NuxtLink>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script setup lang="ts">
+    defineProps<{
+        links: { to: string, name: string }
+    }>();
+</script>
