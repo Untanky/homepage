2fb4bf50d3df54ef528a40f08ed862f14472646d
Lukas Grimm <lukaskingsmail@gmail.com>
2024-01-16 15:54:13 +0100

    Implement individual project page

--- pages/projects/[title].vue
+++ pages/projects/[title].vue
@@ -1,3 +1,27 @@
 <template>
-    Project Page
+    <main class="max-w-[820px] mx-4 md:mx-auto">
+        <img
+            v-if="project?.previewImage"
+            class="w-full rounded-xl"
+            :src="project?.previewImage.source"
+            :alt="project?.previewImage.alt"
+        >
+        <ContentRenderer
+            class="mt-8 prose lg:prose-xl dark:prose-invert prose-zinc prose-a:decoration-rose-300"
+            :value="project!"
+        />
+    </main>
 </template>
+
+<script setup lang="ts">
+    const { params } = useRoute();
+
+    const { data: project } = await useAsyncData(`projects/${params.title}`, () => queryContent('projects', params.title as string).findOne());
+    
+    useHead({
+        title: project.value?.title,
+        meta: [
+            { name: 'description', content: project.value?.description }
+        ]
+    });
+</script>
