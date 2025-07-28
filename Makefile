b3e06ad141261a1bfe5fb5c29a4e84b4c38360c3
Lukas Grimm <lukaskingsmail@gmail.com>
2025-07-28 07:51:02 +0200

    Implemented basic landing page

---
+++ Makefile
@@ -0,0 +1,15 @@
+clean:
+	rm -rf dist
+
+tailwind-build:
+	npx @tailwindcss/cli -i assets/index.css -o dist/assets/index.css
+
+templ-generate:
+	templ generate
+
+go-build: clean tailwind-build templ-generate
+	go build -o dist/server ./...
+
+dev: go-build
+	cd dist && ./server
+
---
+++ cmd/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+)
+
+type staticHandler struct {
+	prefix string
+	handler http.Handler
+}
+
+type MainHandler struct {
+	staticHandlers []staticHandler
+}
+
+func (handler *MainHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	for _, h := range handler.staticHandlers {
+		if s, ok := strings.CutPrefix(request.URL.Path, h.prefix); ok {
+			request.URL.Path = s
+
+			h.handler.ServeHTTP(writer, request)
+			return
+		}
+	}
+
+	writer.WriteHeader(200)
+	model := layoutModel {
+		title: "Lukas Grimm",
+	}
+	layout(model, landingPage()).Render(request.Context(), writer)
+}
+
+func (handler *MainHandler) RegisterStatic(httpPrefix string, osPath string) {
+	fileServer := http.FileServer(http.Dir(osPath))
+
+	handler.staticHandlers = append(handler.staticHandlers, staticHandler{
+		prefix: httpPrefix,
+		handler: fileServer,
+	})
+}
+
+func main() {
+	myHandler := &MainHandler{}
+	
+	myHandler.RegisterStatic("/assets", "./assets")
+	myHandler.RegisterStatic("/static", "../static")
+
+	server := &http.Server {
+		Addr: ":8080",
+		Handler: myHandler,
+	}
+		
+	println("Listening on port 8080...")
+	err := server.ListenAndServe()
+	panic(err)
+}
+
